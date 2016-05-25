7b86ad43d4f06ca5e3a7c2d7fe001a6e533e8646
Mosharaf Tanvir <mosharartanvir7@gmail.com>
2016-05-25 08:09:23 +0600

    All Styles are completed. Now time to do responsive.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,30 @@ $(function(){
        fitToBackground: true,
        videoId: 'LSmgKRx5pBo'
    });
+
+////////////////////////////////////////
+
+  $('.owl-carousel').owlCarousel({
+    thumbs: true,
+    thumbsPrerendered: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    loop: true
+  });
+
+
+    $(".sticker").sticky({topSpacing:0});
+
+    // jQuery smooth scroll
+        $('.smooth-scroll a').bind('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '0';
+            $('html, body').stop().animate({
+                scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 600, 'easeInOutExpo');
+
+            event.preventDefault();
+        });
+
 });
